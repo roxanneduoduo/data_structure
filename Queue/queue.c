c16abdc5d0b558d629e4bf0d867559a5cf32b358
roxanneduoduo <roxanne_duoduo@hotmail.com>
2019-04-17 15:16:59 +0800

    first commit

---
+++ Queue/queue.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define OK 0
+#define OVERFLOW 100
+#define ERROR 101
+
+
+typedef int QElemType;
+
+typedef struct QNode {
+	QElemType data;
+	struct QNode *next;
+}QNode, *QueuePtr;
+
+typedef struct {
+	QueuePtr front, rear;
+} LinkQueue;
+
+int EnQueue(LinkQueue *Q, QElemType e) {
+	QueuePtr s = (QueuePtr)malloc(sizeof(QNode));
+	
+	if (!s)
+		exit(OVERFLOW);
+	s->data = e;
+	s->next=NULL;
+	Q->rear->next = s;
+	Q->rear = s;
+	
+	return OK;
+}
+
+int DeQueue(LinkQueue *Q, QElemType *e) {
+	QueuePtr p;
+	if (Q->front == Q->rear)
+		return ERROR;
+	p = Q->front->next;
+	*e = p->data;
+	Q->front->next = p->next;
+	
+	if (Q->rear == p)
+		Q->rear = Q->front;
+	free(p);
+	return OK;
+}
+
+int main(int argc, char* argv[])
+{
+	LinkQueue *myQueue;
+	int i, *e;
+	
+	for (i=0; i < 10; i++){
+		EnQueue(myQueue, i);
+		printf("%d is in\n", i);
+	}
+	
+	for (i=9; i < 0; i--) {
+		DeQueue(myQueue, e);
+		printf("%d is out\n", *e);
+	}
+}
+
+
